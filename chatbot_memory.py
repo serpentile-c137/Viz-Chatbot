3e75501f2c63c85d43750a2ef8c5f6e96388ae8d
Shardul Gore <sgore@delphime.com>
2025-10-17 12:43:01 +0530

    chatbot memory+tool

--- chatbot_memory.py
+++ chatbot_memory.py
@@ -4,29 +4,52 @@ from langchain_groq import ChatGroq
 from langchain_core.messages import AIMessage, HumanMessage
 from dotenv import load_dotenv
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import ToolNode
+from datetime import datetime
 
 load_dotenv()
 
 memory = MemorySaver()
 
-llm = ChatGroq(model="llama-3.1-8b-instant")
-
 class BasicChatState(TypedDict): 
     messages: Annotated[list, add_messages]
+
+def date_tool(query: str | None = None) -> str:
+    """
+    Return today's date as an ISO-formatted string. 
+    """
+    return datetime.now().date().isoformat()
+
+tools = [date_tool]
+
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+llm_with_tools = llm.bind_tools(tools=tools)
 
 def chatbot(state: BasicChatState): 
     return {
        "messages": [llm.invoke(state["messages"])]
     }
 
+def tools_router(state: BasicChatState):
+    last_message = state["messages"][-1]
+
+    if(hasattr(last_message, "tool_calls") and len(last_message.tool_calls) > 0):
+        return "tool_node"
+    else: 
+        return END
+
+
+tool_node = ToolNode(tools=tools)
 graph = StateGraph(BasicChatState)
 
 graph.add_node("chatbot", chatbot)
-
-graph.add_edge("chatbot", END)
+graph.add_node("tool_node", tool_node)
+# graph.add_edge("chatbot", END)
 
 graph.set_entry_point("chatbot")
-
+graph.add_conditional_edges("chatbot", tools_router)
+graph.add_edge("tool_node", "chatbot")
 app = graph.compile(checkpointer=memory)
 
 config = {"configurable": {
